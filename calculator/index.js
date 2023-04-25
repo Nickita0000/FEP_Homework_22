162953189a6ff5e910a5fbf09bf08acff0cbfccc
Nickita0000 <nzadubniak@gmail.com>
2023-04-25 20:57:20 +0100

    Simple calculator with modules

---
+++ calculator/index.js
@@ -0,0 +1,9 @@
+const sub = require('./sub.js');
+const mult = require('./mult.js');
+const div = require('./div.js');
+const add = require('./add.js');
+
+module.exports.calculator = {sub, mult, div, add}
+
+
+
